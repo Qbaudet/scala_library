dfdf50390ac96c7b175c8dff48320930a7f36e83
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2025-07-25 23:27:49 +0200

    Merge partiel depuis memberMenu : fichier DateTimeCodecs.scala

---
+++ src/main/scala/utils/DateTimeCodecs.scala
@@ -0,0 +1,29 @@
+package utils
+
+import upickle.default.{ReadWriter, readwriter}
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * Fournit les instances de sérialisation et désérialisation (ReadWriter) pour
+ * java.time.LocalDateTime afin qu'il puisse être pris en charge par uPickle.
+ *
+ * Utilise le format ISO_LOCAL_DATE_TIME pour convertir les dates en chaîne et vice versa.
+ */
+object DateTimeCodecs:
+  private val fmt = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+  
+  /**
+   * Instance uPickle pour convertir LocalDateTime en chaîne ISO et inversement.
+   *
+   * - Sérialisation : LocalDateTime -> String avec format ISO_LOCAL_DATE_TIME
+   * - Désérialisation : String -> LocalDateTime en analysant le format ISO
+   */
+  given rwLocalDateTime: ReadWriter[LocalDateTime] =
+  readwriter[String].bimap(
+    // Transforme: LocalDateTime -> String
+    dt => dt.format(fmt),
+    // Lit: String -> LocalDateTime
+    str => LocalDateTime.parse(str, fmt)
+  )
+
